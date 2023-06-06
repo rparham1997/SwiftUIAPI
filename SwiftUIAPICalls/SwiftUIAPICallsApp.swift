cd3062c9cb69dcab05e5b6247c7a741b66040f36
rparham1997 <rparham1997@gmail.com>
2023-06-06 14:33:55 -0400

    Initial Commit

---
+++ SwiftUIAPICalls/SwiftUIAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallsApp.swift
+//  SwiftUIAPICalls
+//
+//  Created by Ramar Parham on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
